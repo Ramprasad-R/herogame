48e0aea91503ce3026c6650bcc14e9bd2df00400
Ramprasad-R <ramprasad.info@gmail.com>
2020-01-10 22:50:35 +0100

    Initial js validation complted

--- herogame.js
+++ herogame.js
@@ -1,11 +1,45 @@
-// Write your JS here
 const hero = {
   name: "Hero",
   heroic: true,
   inventory:[],
   health: 10,
   weapon: {
-    type: "Sword",
+    type: "sword",
     damage: 2
   }
+}
+
+function rest(person){
+  if (person.health === 10){
+    alert("You are already healthy");
+    return null;
+  } else {
+    person.health = 10;
+  }
+  return person
+}
+
+function pickUpItem(person, weapon){
+  person.inventory.push(weapon);
+  return person
+}
+
+function equipWeapon(person){
+  if (person.inventory.length !== 0) {
+    person.weapon = person.inventory[0];
+  } 
+  return person;
+}
+
+function invokeRest() {
+  rest(hero);
+};
+
+function invokePickUpItems() {  
+  pickUpItem(hero, {type: "dagger",
+  damage: 2})
+}
+
+function invokeEquipWepon(){
+  equipWeapon(hero)
 }
